139ab5b3491852e4b5dc0a5cbe730a9390fdf050
Eunji <kej2971@gmail.com>
2023-09-10 19:23:19 +0900

    [FEATURE][LOGIN] 로그인, 회원가입, 페이지 구현

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -6,10 +6,8 @@ import {Navbar, Nav, Container} from 'react-bootstrap'
 const Footer = () => {
   return(
     <footer>
-      <Container>
-            <Nav className="ml-auto">
-              <Nav.Link>로고</Nav.Link>
-            </Nav>
+      <Container id="footer">
+            <Nav className="ml-auto"></Nav>
         <div style={{textAlign:'center'}}>
           Footer
         </div>
